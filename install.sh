a2983913a3128719d481f139fb710b30e63dd2f6
AsueDerick <asuederipotas7@gmail.com>
2024-09-15 21:28:19 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,5 +28,4 @@ sudo usermod -aG docker ubuntu && newgrp docker
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 minikube start --driver=docker
-minikube config set driver docker
 minikube start
