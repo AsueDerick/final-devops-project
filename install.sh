fd6ada8ef3544b31b4d4ebaf80e472b488fb6298
AsueDerick <asuederipotas7@gmail.com>
2024-09-15 18:58:37 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,5 +10,6 @@ sudo apt-get install -y ansible
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-minikube start --driver=none
+minikube start 
+minikube status
 sudo systemctl daemon-reload
