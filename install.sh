97a38d0ff2635cf76b0cd9f0cb0190e528cc111c
AsueDerick <asuederipotas7@gmail.com>
2024-09-15 18:46:55 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo apt-get update
+sudo apt-get install docker.io
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
 sudo systemctl start docker
@@ -21,4 +10,5 @@ sudo apt-get install -y ansible
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-minikube start
+minikube start --driver=none
+sudo systemctl daemon-reload
