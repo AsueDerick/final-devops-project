5e41b473528970e2d56fb58d13f53ed9ee696f57
AsueDerick <asuederipotas7@gmail.com>
2024-09-20 23:06:48 +0400

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -27,8 +27,7 @@ sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
 docker --version
 
-sudo usermod -aG docker ubuntu
-newgrp docker
+sudo usermod -aG docker $USER && newgrp docker
 
 minikube start 
 minikube status
